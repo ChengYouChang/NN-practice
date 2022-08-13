d64a20aac26b849d757a97b522669aa40d8c3d76
ChengYouChang <38828950+ChengYouChang@users.noreply.github.com>
2022-08-13 23:20:58 +0800

    add myNN

---
+++ myNN/test/xor_keras_test.py
@@ -0,0 +1,24 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LeakyReLU, Conv2D
+from keras.optimizers import RMSprop
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
+from matplotlib.pyplot import imshow
+import numpy as np
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
